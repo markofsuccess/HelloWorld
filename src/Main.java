440419f293257d68ced956e8e892623ef5dfb9f4
MarkusTuressonSjöman <markussjoman1@gmail.com>
2022-08-22 13:24:41 +0200

    changed name of change to change 2 in the printout

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!, change");
+        System.out.println("Hello world!, change2");
     }
 }
